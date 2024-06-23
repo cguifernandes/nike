383b6799d140d7d5bb7c1223a248afd2c8b1d7c8
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2024-06-22 21:51:03 -0300

    feat: starting project

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const init = () => {
+	const appContainer = document.createElement("div");
+	if (!appContainer) throw new Error("Can not find AppContainer");
+
+	document.body.appendChild(appContainer);
+	const root = createRoot(appContainer);
+	appContainer.id = "app";
+	root.render(
+		<>
+			<App />
+		</>,
+	);
+};
+
+init();
